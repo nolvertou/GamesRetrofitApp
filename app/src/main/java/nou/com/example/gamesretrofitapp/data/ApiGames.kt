1dd40e8f14040dec19afe2bffbd0d2ad17d11ab2
nolvertou <nolvertou@gmail.com>
2025-08-18 22:12:04 -0700

    Implementing Model and interface

--- app/src/main/java/nou/com/example/gamesretrofitapp/data/ApiGames.kt
+++ app/src/main/java/nou/com/example/gamesretrofitapp/data/ApiGames.kt
@@ -1,4 +1,14 @@
 package nou.com.example.gamesretrofitapp.data
 
+import nou.com.example.gamesretrofitapp.model.GamesModel
+import nou.com.example.gamesretrofitapp.util.Constants.Companion.API_KEY
+import nou.com.example.gamesretrofitapp.util.Constants.Companion.ENDPOINT
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiGames {
+
+    @GET(ENDPOINT + API_KEY)
+    suspend fun getGames(): Response<GamesModel>
+
 }
