1a43ea87b3faf2d13ce69add302244ff42c535aa
nolvertou <nolvertou@gmail.com>
2025-08-19 21:29:39 -0700

    Create Card Game and Show Card of the game in Home View

--- app/src/main/java/nou/com/example/gamesretrofitapp/components/BodyComponents.kt
+++ app/src/main/java/nou/com/example/gamesretrofitapp/components/BodyComponents.kt
@@ -1,18 +1,31 @@
-package nou.com.example.gamesretrofitapp
+package nou.com.example.gamesretrofitapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import nou.com.example.gamesretrofitapp.model.GameList
 import nou.com.example.gamesretrofitapp.util.Constants.Companion.CUSTOM_BLACK
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,4 +52,36 @@ fun MainTopBar(
             }
         }
     )
+}
+
+@Composable
+fun MainImage(image: String){
+    val image = rememberAsyncImagePainter(model = image)
+
+    Image(
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    )
+}
+
+@Composable
+fun CardGame(
+    game: GameList,
+    onClick: () -> Unit
+){
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .shadow(40.dp)
+            .clickable { onClick() }
+    ){
+        Column() {
+            MainImage(image = game.background_image)
+        }
+    }
 }
--- app/src/main/java/nou/com/example/gamesretrofitapp/views/HomeView.kt
+++ app/src/main/java/nou/com/example/gamesretrofitapp/views/HomeView.kt
@@ -1,7 +1,7 @@
 package nou.com.example.gamesretrofitapp.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,8 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import nou.com.example.gamesretrofitapp.MainTopBar
+import nou.com.example.gamesretrofitapp.components.CardGame
+import nou.com.example.gamesretrofitapp.components.MainTopBar
+import nou.com.example.gamesretrofitapp.util.Constants.Companion.CUSTOM_BLACK
 import nou.com.example.gamesretrofitapp.viewModel.GamesViewModel
 
 @Composable
@@ -39,9 +43,21 @@ fun ContentHomeView(
 ){
     val games by viewModel.games.collectAsState()
 
-    LazyColumn( modifier = Modifier.padding(padding).padding(20.dp)) {
+    LazyColumn( modifier = Modifier
+        .padding(padding)
+        //.padding(20.dp)
+        .background(Color(CUSTOM_BLACK))
+    ) {
         items(games){ item ->
-            Text(text = item.name)
+            CardGame(item){
+                // TODO
+            }
+            Text(
+                text = item.name,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(start = 10.dp)
+            )
         }
     }
 }
