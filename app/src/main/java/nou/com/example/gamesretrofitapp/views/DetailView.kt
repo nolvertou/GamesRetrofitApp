d492dd013c1fe531187e2f06009f3efd1d43773b
nolvertou <nolvertou@gmail.com>
2025-08-29 07:43:49 -0700

    Add description with Scroll in text description

--- app/src/main/java/nou/com/example/gamesretrofitapp/views/DetailView.kt
+++ app/src/main/java/nou/com/example/gamesretrofitapp/views/DetailView.kt
@@ -9,12 +9,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import nou.com.example.gamesretrofitapp.components.MainImage
@@ -75,6 +78,16 @@ fun ContentDetailView(
             MetaWebsite(url = state.website)
             ReviewCard(state.metacritic)
         }
+
+        val scroll = rememberScrollState(0)
+        Text(
+            text = state.description_raw,
+            color = Color.White,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 15.dp, end = 15.dp, bottom = 10.dp)
+                .verticalScroll(scroll)
+        )
     }
 
 
